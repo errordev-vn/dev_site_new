a138b3fca9baa90ca1743911fbba805d83d6a581
Velo <105860237+velo-app[bot]@users.noreply.github.com>
2024-10-18 13:06:29 +0000

    Initial commit

---
+++ src/pages/Trang mới.wihb5.js
@@ -0,0 +1,15 @@
+/ Tham chiếu API Velo: https://www.wix.com/velo/reference/api-overview/introduction
+
+$w.onReady(function () {
+
+	// Viết mã JavaScript của bạn vào đây bằng cách sử dụng API khung Velo
+
+	// Hiển thị nội dung Xin chào:
+	// console.log("Xin chào!");
+
+	// Lệnh gọi trong các phần tử của trang, ví dụ như:
+	// $w("#button1").label = "Nhấp vào đây!";
+
+	// Nhấp vào "Chạy" hoặc Xem trước trang web để chạy mã của bạn
+
+});
